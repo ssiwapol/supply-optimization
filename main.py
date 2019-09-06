c97f23db327377480822586efe6ce71778ab29b6
Siwapol Techaratsami <SIWAPOLT@cementhai.com>
2019-09-06 09:41:53 +0700

    change port

--- main.py
+++ main.py
@@ -40,4 +40,4 @@ tab_viz.set_callbacks(app)
 
 
 if __name__ == '__main__':
-    app.run_server(debug=configinfo['app']['debug'], host='0.0.0.0')
+    app.run_server(debug=configinfo['app']['debug'], host='0.0.0.0', port=8080)
